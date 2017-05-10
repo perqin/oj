8d3c6d05eb4baca6b4459c8771110ca2f6d56d4a
perqin <perqinxie@gmail.com>
2017-05-10 21:04:27 +0800

    Add lintcode/insertion-sort-list

---
+++ lintcode/insertion-sort-list/InsertionSortList.java
@@ -0,0 +1,33 @@
+public class InsertionSortList {
+	private static ListNode buildList(int... numbers) {
+		ListNode list = null;
+		ListNode tail = null;
+		for (int number : numbers) {
+		 	ListNode node = new ListNode(number);
+			node.next = null;
+			if (list == null) {
+				list = node;
+			} else {
+				tail.next = node;
+			}
+			tail = node;
+		}
+		return list;
+	}
+
+	private static void printList(ListNode list) {
+		while (list != null) {
+			System.out.print(list.val + " -> ");
+			list = list.next;
+		}
+		System.out.println("null");
+	}
+
+	public static void main(String[] args) {
+		Solution solution = new Solution();
+		ListNode list = buildList(1, 3, 2, 0);
+		printList(list);
+		ListNode sortedList = solution.insertionSortList(list);
+		printList(sortedList);
+	}
+}
---
+++ lintcode/insertion-sort-list/Solution.java
@@ -0,0 +1,41 @@
+public class Solution {
+    /**
+     * @param head: The first node of linked list.
+     * @return: The head of linked list.
+     */
+    public ListNode insertionSortList(ListNode head) {
+        if (head == null) {
+            return head;
+        }
+        ListNode prePicked = head;
+        ListNode picked = head.next;
+        while (picked != null) {
+            // Pick one node! Now pick first bigger node
+            if (head.val > picked.val) {
+                // Head picked and should be replaced
+                prePicked.next = picked.next;
+                picked.next = head;
+                head = picked;
+                picked = prePicked.next;
+            } else {
+                ListNode newPrePicked = head;
+                ListNode newPicked = head.next;
+                while (newPicked.val < picked.val) {
+                    newPrePicked = newPrePicked.next;
+                    newPicked = newPicked.next;
+                }
+                if (newPicked != picked) {
+                    // We pick a new bigger one
+                    prePicked.next = picked.next;
+                    picked.next = newPicked;
+                    newPrePicked.next = picked;
+                    picked = prePicked.next;
+                } else {
+                    prePicked = prePicked.next;
+                    picked = picked.next;
+                }
+            }
+        }
+        return head;
+    }
+}
