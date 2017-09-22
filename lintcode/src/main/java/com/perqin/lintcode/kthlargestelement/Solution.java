22cd1879195d34aa2c9c74566919865b15d10d6a
perqin <perqinxie@gmail.com>
2017-09-23 01:43:47 +0800

    AC:lintcode/kth-largest-element

--- lintcode/src/main/java/com/perqin/lintcode/kthlargestelement/Solution.java
+++ lintcode/src/main/java/com/perqin/lintcode/kthlargestelement/Solution.java
@@ -1,45 +1,47 @@
 package com.perqin.lintcode.kthlargestelement;
 
 class Solution {
-  /**
-   * @param k : description of k
-   * @param nums : array of nums
-   * @return description of return
-   */
-  int kthLargestElement(int k, int[] nums) {
-      return kthLargestElement(k, nums, 0, nums.length - 1);
-  }
-  
-  private int kthLargestElement(int k, int[] nums, int left, int right) {
-      System.out.println("k = " + k + ", left = " + left + ", right = " + right);
-      int tmp;
-      if (left == right) {
-          return nums[k - 1];
-      }
-      int pivot = nums[left];
-      int l = left, r = right;
-      while (l < r) {
-          while (l <= right && nums[l] > pivot) {
-              ++l;
-          }
-          while (r >= left && nums[r] < pivot) {
-              --r;
-          }
-          // nums[r] >= pivot >= nums[l]
-          if (l < r) {
-              tmp = nums[r];
-              nums[r] = nums[l];
-              nums[l] = tmp;
-              ++l;
-              --r;
-          }
-      }
-      if (r > left && k - 1 <= r) {
-          return kthLargestElement(k, nums, left, r - 1);
-      }
-      if (k - 1 >= l) {
-          return kthLargestElement(k - l, nums, l, right);
-      }
-      return nums[k];
-  }
-};
+    /**
+     * @param k : description of k
+     * @param nums : array of nums
+     * @return description of return
+     */
+    int kthLargestElement(int k, int[] nums) {
+        return kthLargestElement(k, nums, 0, nums.length - 1);
+    }
+
+    private int kthLargestElement(int k, int[] nums, int left, int right) {
+        System.out.println("Search in [" + left + " - " + right + "] for the " + k + "-th largest number");
+        for (int i = left; i <= right; ++i) System.out.print(nums[i] + ",");
+        System.out.println();
+        int tmp;
+        if (left == right) {
+            return nums[left];
+        }
+        int pivot = nums[left];
+        int l = left;
+        int r = right;
+        while (l < r) {
+            while (l <= right && nums[l] > pivot) {
+                ++l;
+            }
+            while (r >= left && nums[r] < pivot) {
+                --r;
+            }
+            // nums[r] >= pivot >= nums[l]
+            if (l <= r) {
+                tmp = nums[r];
+                nums[r] = nums[l];
+                nums[l] = tmp;
+                ++l;
+                --r;
+            }
+        }
+        System.out.println("l = " + l + ", r = " + r + ", left = " + left + ", right = " + right);
+        if (r >= left && k - 1 <= r) {
+            return kthLargestElement(k, nums, left, r);
+        } else {
+            return kthLargestElement(k, nums, l, right);
+        }
+    }
+}
