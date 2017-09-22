fc0538c6f592a67f6e35d8d31905a2a9a3afcaba
perqin <perqinxie@gmail.com>
2017-09-22 19:50:19 +0800

    Working

--- lintcode/src/main/java/com/perqin/lintcode/kthlargestelement/Solution.java
+++ lintcode/src/main/java/com/perqin/lintcode/kthlargestelement/Solution.java
@@ -1,16 +1,17 @@
 package com.perqin.lintcode.kthlargestelement;
 
-public class Solution {
-  /*
+class Solution {
+  /**
    * @param k : description of k
    * @param nums : array of nums
-   * @return: description of return
+   * @return description of return
    */
-  public int kthLargestElement(int k, int[] nums) {
+  int kthLargestElement(int k, int[] nums) {
       return kthLargestElement(k, nums, 0, nums.length - 1);
   }
   
   private int kthLargestElement(int k, int[] nums, int left, int right) {
+      System.out.println("k = " + k + ", left = " + left + ", right = " + right);
       int tmp;
       if (left == right) {
           return nums[k - 1];
@@ -18,10 +19,10 @@ public class Solution {
       int pivot = nums[left];
       int l = left, r = right;
       while (l < r) {
-          while (l <= right && nums[l] >= pivot) {
+          while (l <= right && nums[l] > pivot) {
               ++l;
           }
-          while (r >= left && nums[r] <= pivot) {
+          while (r >= left && nums[r] < pivot) {
               --r;
           }
           // nums[r] >= pivot >= nums[l]
@@ -33,8 +34,8 @@ public class Solution {
               --r;
           }
       }
-      if (k - 1 <= r) {
-          return kthLargestElement(k, nums, left, r);
+      if (r > left && k - 1 <= r) {
+          return kthLargestElement(k, nums, left, r - 1);
       }
       if (k - 1 >= l) {
           return kthLargestElement(k - l, nums, l, right);
